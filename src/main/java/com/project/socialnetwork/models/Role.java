3aedc68acb627b70eb2c2cb88c187a4607be1dc2
hahahalol9 <cmak7543@mail.ru>
2022-05-07 22:51:41 +0300

    role class added

---
+++ src/main/java/com/project/socialnetwork/models/Role.java
@@ -0,0 +1,25 @@
+package com.project.socialnetwork.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @ManyToMany
+    private Set<User> users;
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+}
