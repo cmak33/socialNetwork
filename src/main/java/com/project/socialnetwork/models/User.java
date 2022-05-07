66f47f9861731d002973b2ab74ff6fc3ad9c9218
hahahalol9 <cmak7543@mail.ru>
2022-05-07 22:50:42 +0300

    user class added

---
+++ src/main/java/com/project/socialnetwork/models/User.java
@@ -0,0 +1,65 @@
+package com.project.socialnetwork.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.util.Collection;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String status;
+    @Email
+    private String email;
+    @ManyToMany(mappedBy = "users")
+    private Set<Role> roles;
+    @OneToMany(mappedBy = "user")
+    private Set<Post> posts;
+    @OneToMany(mappedBy = "user")
+    private Set<Like> likes;
+    @OneToMany(mappedBy = "user")
+    private Set<Comment> comments;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
