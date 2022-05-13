55f689b104ae0957afb0dc7269fb79f7e4752006
hahahalol9 <cmak7543@mail.ru>
2022-05-13 23:56:19 +0300

    user service isUsernameUnique function added

---
+++ src/main/java/com/project/socialnetwork/services/UserService.java
@@ -0,0 +1,70 @@
+package com.project.socialnetwork.services;
+
+
+import com.project.socialnetwork.models.Role;
+import com.project.socialnetwork.models.User;
+import com.project.socialnetwork.repositories.RoleRepository;
+import com.project.socialnetwork.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+@PropertySource("classpath:/properties/user.properties")
+public class UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    @Value("${defaultRole}")
+    private String defaultRole;
+
+    public void saveNewUser(User user){
+        encodeUserPassword(user);
+        addDefaultRoleToUser(user);
+        userRepository.save(user);
+    }
+    private void encodeUserPassword(User user){
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+    }
+
+    private void addDefaultRoleToUser(User user){
+        Set<Role> userRoles = Collections.singleton(roleRepository.findByName(defaultRole));
+        user.setRoles(userRoles);
+    }
+
+    public boolean isUsernameUnique(String username){
+        return userRepository.findByUsername(username) == null;
+    }
+
+
+    public void updateUser(User user){
+        userRepository.save(user);
+    }
+
+    public boolean isCurrentUserId(Long id){
+        return id.equals(receiveCurrentUserId());
+    }
+
+    public Optional<User> findById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public User receiveCurrentUser(){
+        Long id = receiveCurrentUserId();
+        return findById(id).orElse(null);
+    }
+
+    private Long receiveCurrentUserId(){
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ((User)principal).getId();
+    }
+}
