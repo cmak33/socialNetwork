ce879689a2512eb3e51105884021568a3fa58aad
hahahalol9 <cmak7543@mail.ru>
2022-07-15 15:28:16 +0300

    chore: add beans for converters

--- src/main/java/com/project/socialnetwork/configurations/EntityConverterConfiguration.java
+++ src/main/java/com/project/socialnetwork/configurations/EntityConverterConfiguration.java
@@ -1,8 +1,13 @@
 package com.project.socialnetwork.configurations;
 
-import com.project.socialnetwork.models.entities.User;
+import com.project.socialnetwork.repositories.CommentRepository;
+import com.project.socialnetwork.repositories.RateableRecordRepository;
 import com.project.socialnetwork.repositories.UserRepository;
+import com.project.socialnetwork.services.RatingService;
+import com.project.socialnetwork.services.RecordService;
+import com.project.socialnetwork.services.entity_data_transfer_object_converters.RateableRecordConverter;
 import com.project.socialnetwork.services.entity_data_transfer_object_converters.UserConverter;
+import com.project.socialnetwork.services.entity_to_view_representation_converters.RecordToViewRepresentationConverter;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -20,5 +25,17 @@ public class EntityConverterConfiguration {
     @Autowired
     public UserConverter userConverter(UserRepository userRepository){
         return new UserConverter(modelMapper(),userRepository);
+    }
+
+    @Bean
+    @Autowired
+    public RateableRecordConverter recordConverter(CommentRepository commentRepository, RateableRecordRepository recordRepository){
+        return new RateableRecordConverter(modelMapper(),commentRepository,recordRepository);
+    }
+
+    @Bean
+    @Autowired
+    public RecordToViewRepresentationConverter recordToViewRepresentationConverter(RatingService ratingService, RecordService recordService){
+        return new RecordToViewRepresentationConverter(modelMapper(),ratingService,recordService);
     }
 }
